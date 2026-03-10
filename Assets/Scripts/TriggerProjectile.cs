20894d317ff00aad2e0e18e9224706fda44b22ed
Jarrell <jhouston2882@yahoo.com>
2026-03-10 08:42:24 -0500

    Trigger Projectiles

--- Assets/Scripts/TriggerProjectile.cs
+++ Assets/Scripts/TriggerProjectile.cs
@@ -2,12 +2,41 @@ using UnityEngine;
 
 public class TriggerProjectile : MonoBehaviour
 {
-    [SerializeField] GameObject projectile;
+    // [SerializeField] GameObject projectile;
+    [SerializeField] GameObject[] projectiles;
+
+    void Start()
+    {
+        // projectiles = GameObject.FindGameObjectsWithTag("Projectile");
+        // SetProjectilesInactive();
+    }
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.CompareTag("Player"))
         {
-            projectile.SetActive(true);
+            SetProjectilesActive();
+            
+        }
+    }
+
+    void SetProjectilesActive()
+    {
+        foreach (GameObject projectile in projectiles)
+        {
+            if (projectile)
+            {
+                projectile.SetActive(true);
+                
+            }
+        }
+    }
+
+    void SetProjectilesInactive()
+    {
+        foreach (GameObject projectile in projectiles)
+        {
+            projectile.SetActive(false);
         }
     }
 }
