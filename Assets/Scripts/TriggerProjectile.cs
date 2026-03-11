a470c75d5cebf3d4d4e4c8e60b845a5136243e9c
Jarrell <jhouston2882@yahoo.com>
2026-03-10 21:05:35 -0500

    Prefabs and Level Build

--- Assets/Scripts/TriggerProjectile.cs
+++ Assets/Scripts/TriggerProjectile.cs
@@ -2,39 +2,52 @@ using UnityEngine;
 
 public class TriggerProjectile : MonoBehaviour
 {
-    // [SerializeField] GameObject projectile;
     [SerializeField] GameObject[] projectiles;
+    [SerializeField] GameObject[] projectiles1;
 
+    void Awake()
+    {
+        SetProjectilesInactive(projectiles);
+        SetProjectilesInactive(projectiles1);
+    }
     void Start()
     {
-        // projectiles = GameObject.FindGameObjectsWithTag("Projectile");
-        // SetProjectilesInactive();
     }
 
     void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.CompareTag("Player"))
+        if (collider.gameObject.CompareTag("Player") && gameObject.CompareTag("Trigger"))
         {
-            SetProjectilesActive();
+            SetProjectilesActive(projectiles);
             
+        }
+
+        if (collider.gameObject.CompareTag("Player") && gameObject.CompareTag("Trigger2"))
+        {
+            Debug.Log("Entered trigger 2");
+            SetProjectilesActive(projectiles1);
+            Debug.Log("Entered 2 after call");
         }
     }
 
-    void SetProjectilesActive()
+    void SetProjectilesActive(GameObject[] projectileArg)
     {
-        foreach (GameObject projectile in projectiles)
+        Debug.Log("Call the 2nd function???");
+        foreach (GameObject projectile in projectileArg)
         {
+            Debug.Log("Triggered Loop?");
             if (projectile)
             {
+                Debug.Log("Projectile??");
                 projectile.SetActive(true);
                 
             }
         }
     }
 
-    void SetProjectilesInactive()
+    void SetProjectilesInactive(GameObject[] projectileArg)
     {
-        foreach (GameObject projectile in projectiles)
+        foreach (GameObject projectile in projectileArg)
         {
             projectile.SetActive(false);
         }
